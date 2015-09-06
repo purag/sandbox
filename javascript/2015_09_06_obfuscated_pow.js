a3c33551cab57058f65c8608c68b502cb9499589
Purag Moumdjian <purmou@gmail.com>
2015-09-06 05:01:11 -0700

    maybe kinda obfuscated-ish power function

---
+++ javascript/2015_09_06_obfuscated_pow.js
@@ -0,0 +1,11 @@
+/* Power function
+ * Recursive power function with stupid variable/parameter names.
+ */
+
+var _ = (___,__) => __?_(___,--__)*___:++__;
+
+/* Usage:
+ * 
+ * _(2,5) = 32
+ * _(3,3) = 27
+ */
